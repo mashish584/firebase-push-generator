8cd9ed1395fddb29074539bc4281f6646b4e0b0e
Ashish Mehra <singhashu163@gmail.com>
2021-02-15 18:38:35 +0530

    Firebase token input created for generating push

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,22 +1,29 @@
 const { sendPushNotification } = require("../handlers/firebase");
 
 exports.getIndex = (req, res, next) => {
-	return res.render("index", { title: "Node Application Boilerplate" });
+  return res.render("index", { title: "Node Application Boilerplate" });
 };
 
-exports.getPushNotification = (req,res,next) => {	
-	const {token} = req.query;
-	sendPushNotification({
-		notification:{
-			title : "New Message",
-			body : "Testing Push Notification",
-		},
-		data:{
-			title : "NEW MESSAGE",
-			body:"CUSTOME"
-		},
-		  
-		token 
-	})
-	return res.send("Push Notification successfully generated.");
+exports.getPushNotification = async (req, res, next) => {
+  const { token } = req.query;
+  try {
+    await sendPushNotification({
+      notification: {
+        title: "New Message",
+        body: "Testing Push Notification",
+      },
+      data: {
+        title: "New Message",
+        body: "Testing Push Notification",
+        openURL: "https://google.com",
+        click_action: "https://google.com",
+      },
+
+      token,
+    });
+    return res.send("Push Notification successfully generated.");
+  } catch (err) {
+    return res.send("Error in generating push.");
+  }
 };
+``;
